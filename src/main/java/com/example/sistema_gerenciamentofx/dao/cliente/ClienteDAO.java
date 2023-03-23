bb963ca4b5355325b165cdbd8ba1d78dda7553c3
Naila <nailasuely@users.noreply.github.com>
2023-03-23 02:48:43 -0300

    Versao_2.2.3 - DAO Cliente criado com alguns métodos; - Menu simples para testar a classe cliente; - UIID adicionado para criação de um id único. (No entanto, ainda precisa de revisão.)

--- src/main/java/com/example/sistema_gerenciamentofx/dao/cliente/ClienteDAO.java
+++ src/main/java/com/example/sistema_gerenciamentofx/dao/cliente/ClienteDAO.java
@@ -7,7 +7,7 @@ import java.util.ArrayList;
 
 public interface ClienteDAO extends CRUD<Cliente> {
     public Cliente findByCPF(String cpf);
-    public Cliente create(ArrayList<Cliente> clientes,Cliente client, String adress, String CPF,String nome, int telefone);
+    // public Cliente create(ArrayList<Cliente> clientes,Cliente client, String adress, String CPF,String nome, int telefone);
     public boolean findByCpfIsTrue(String cpf);
 
 
