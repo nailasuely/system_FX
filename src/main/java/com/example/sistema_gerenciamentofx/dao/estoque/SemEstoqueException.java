b7be39f208012127374cb035f4655d389c582c0c
Naila <nailasuely@users.noreply.github.com>
2023-04-03 14:11:54 -0300

    Versao_2.3.4 - Classe Produto adicionada no model. - Classe Estoque adicionada no DAO. - Alguns testes implementados para estoque. - Mudanças na classe ordem de serviço.

---
+++ src/main/java/com/example/sistema_gerenciamentofx/dao/estoque/SemEstoqueException.java
@@ -0,0 +1,8 @@
+package com.example.sistema_gerenciamentofx.dao.estoque;
+
+public class SemEstoqueException extends Exception {
+    public SemEstoqueException(String mensagem) {
+        super(mensagem);
+    }
+}
+
