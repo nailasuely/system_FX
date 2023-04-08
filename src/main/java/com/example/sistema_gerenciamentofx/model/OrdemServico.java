31385c73e7c8bb5f790bbb8c09e02e8935efdeab
Rhian Pablo <rhianpablo11@gmail.com>
2023-04-08 13:57:12 -0300

    Versão_2.4.2
    -adicionado novos elementos do JavaDoc em Ordem de Serviço

--- src/main/java/com/example/sistema_gerenciamentofx/model/OrdemServico.java
+++ src/main/java/com/example/sistema_gerenciamentofx/model/OrdemServico.java
@@ -103,15 +103,68 @@ public class OrdemServico {
      * </ul>
      */
     private String paymentType;
-    
+    /**
+     *O atributo <b>clientSatisfaction</b>, do tipo <i>int</i>, serve para poder armazenar a nota dada pelo cliente sobre a satisfação do mesmo com relação ao serviço realizado
+     */
     private int clientSatisfaction;
+    /**
+     * O atributo <b>status</b>, do tipo String, serve para armazenar qual status da ordem naquele momento<br>
+     * Eles podem ser:
+     * <ul>
+     *     <li>
+     *         "em espera"
+     *     </li>
+     *     <li>
+     *         "em andamento"
+     *     </li>
+     *     <li>
+     *         "cancelada"
+     *     </li>
+     *     <li>
+     *         "finalizada"
+     *     </li>
+     * </ul>
+     */
     private String status;
+    /**
+     * O atributo <b>expendTime</b>, do tipo <i>String</i>, serve para poder armazenar o tempo que passou até a
+     * ordem ser finalizada, partindo do momento da sua crioção
+     */
     private String expendTime;
+    /**
+     * O atributo <b>description</b>, do tipo <i>String</i>, serve para armazer a descrição daquele serviço, contendo
+     * informações como:<ul>
+     *     <li>
+     *         Preço do serviço prestado
+     *     </li>
+     *     <li>
+     *         Serviços e peças que foram escolhidas pelo cliente
+     *     </li>
+     *     <li>
+     *         Tempo de duração da ordem de serviço
+     *     </li>
+     *     <li>
+     *         Informações de identificação do tecnico e do cliente
+     *     </li>
+     *     <li>
+     *         Forma de pagamento escolhida pelo usuario
+     *     </li>
+     *     <li>
+     *         Satisfação do cliente com o serviço
+     *     </li>
+     * </ul>
+     */
     private String description;
 
+    /**
+     * Método construtor da classe, o qual já preenche os atributos: <b>start, status</b>. Além de iniciar a
+     * lista que conterá produtos/peças.<br>
+     * Essa lista pode ser vazia, a depender do serviço escolhido pelo cliente;
+     */
     public OrdemServico() {
         if(start == null){
             start = LocalDate.now();
+            end = null;
         }
         if(produtoLists == null){
             produtoLists = new ArrayList<>();
@@ -121,6 +174,7 @@ public class OrdemServico {
         }
     }
 
+    //PERGUNTAR A NAI
     public void setListaProdutos(Produto produto, int quantidade) throws SemEstoqueException, ProdutoErradoException {
         // caso seja outro tipo de produto, isso será adicionado no if depois
         if(produto.getNome().equals("ram") ||produto.getNome().equals("placa mae") ||
@@ -134,62 +188,121 @@ public class OrdemServico {
 
     }
 
+    /**
+     * Método para obter a lista de produtos presente na ordem de serviço
+     * @return <i>List</i> que contém os produtos presente na lista. Lembrando que ela pode estar vazia a depender do serviço escolhido
+     */
     public List<Produto> getProdutoLists() {
         return produtoLists;
     }
 
+    /**
+     * Serve para preencher com ID da ordem, este que é gerado aleatoriamente no método construtor da classe.
+     * @param id Valor de ID para preencher o atributo
+     */
     public void setId(String id) {
         this.id = id;
     }
 
+    /**
+     * Método para obter o ID que esta vinculado a ordem de serviço
+     * @return <i>String</i> contendo o valor do ID vinculado a ordem de serviço
+     */
     public String getId() {
         return id;
     }
 
+    /**
+     * Método para obter a data em que foi iniciada a ordem de serviço
+     * @return Objeto do tipo <i>LocalDate</i> contendo a data no formato YYYY-MM-DD
+     */
     public LocalDate getStart() {
         return start;
     }
 
+    /**
+     * Serve para preencher com valor da data em que foi iniciada a ordem de serviço
+     * @param start Objeto do tipo <i>LocalDate</i> contendo uma data
+     */
     public void setStart(LocalDate start) {
         this.start = start;
     }
 
+    /**
+     * Método serve para obter a data em que foi completada a ordem.
+     * @return Objeto do tipo <i>LocalDate</i> contendo a data em que a ordem foi finalizada, mas caso ainda esteja em andamento
+     * ela terá um retorno nulo
+     */
     public LocalDate getEnd() {
         return end;
     }
 
-    public void setEnd(LocalDate end) {
+    /**
+     * Serve para definir a data em que a ordem foi finalizada, ou cancelada.<br>
+     * Valor preenchido automaticamente ao chamar o método
+     */
+    public void setEnd() {
         this.end = LocalDate.now();
     }
 
+    /**
+     * Método para obter o ID do cliente que requeriu a ordem de serviço
+     * @return <i>String</i> contendo o ID do cliente
+     */
     public  String getClientId() {
         return clientId;
     }
 
+    /**
+     * Serve para preencher o atributo do ID do cliente, e então saber quem requisitou o serviço contido nela
+     * @param clientId <i>String</i> contendo o valor do ID do cliente
+     */
     public void setClientId(String clientId) {
         this.clientId = clientId;
     }
-
+    /**
+     * Método para obter o ID do tecnico que cadastrou a ordem de serviço e irá trabalhar nela
+     * @return <i>String</i> contendo o ID do tecnico
+     */
     public String getTechnicianID() {
         return technicianID;
     }
-
+    /**
+     * Serve para preencher o atributo do ID do tecnico que vai trabalhar na ordem de serviço
+     * @param clientId <i>String</i> contendo o valor do ID do cliente
+     */
     public void setTechnicianID(String technicianID) {
         this.technicianID = technicianID;
     }
 
+    /**
+     * Método para obter o tipo do serviço contratado pelo cliente, seja "formatação", ou "montagem/instalação", ou "limpeza", ou "instalação de programas"
+     * @return Objeto do tipo <i>Produto</i> contendo a informação do tipo do serviço
+     */
     public Produto getType() {
         return type;
     }
 
+    /**
+     * Serve para preencher o atributo do tipo do serviço que foi contratado pelo cliente
+     * @param type Objeto do tipo <i>Produto</i> contendo informação do tipo do serviço
+     */
     public void setType(Produto type) {
         this.type = new Produto();
     }
 
+    /**
+     * Método para obter o preço total dos serviços, e/ou produtos, presentes naquela ordem de serviço
+     * @return <i>Double</i> contendo um valor que representa o preço daquele serviço
+     */
     public double getPrice() {
         return price;
     }
 
+    /**
+     * Serve para preencher o atributo <b>price</b>, que guarda o preço do serviço
+     * @param price Variavel do tipo <i>double</i>, para ser trabalhada na lógica interna de preenchimento do preço
+     */
     public void setPrice(double price) {
         // Lembrando que nessa lista só é adicionada PRODUTOS/PEÇAS, só vai ser maior doq
         // zero se for uma montagem.
@@ -205,10 +318,19 @@ public class OrdemServico {
         this.price = price;
     }
 
+    /**
+     * Método para obter o tipo do pagamento que foi escolhido pelo cliente, para pagar pelo serviço contratado
+     * @return <i>String</i> contendo a forma escolhida para realizar o pagamento
+     */
     public String getPaymentType() {
         return paymentType;
     }
 
+    /**
+     * Serve para preencher o atributo do tipo de pagamento, <b>paymentType</b>.<br>
+     * Antes de realizar o preenchimento, ele verifica se o tipo enviado é valido, e caso este não seja, ocorre uma exceção
+     * @param paymentType <i>String</i> contendo a forma de pagamento escolhida pelo cliente para pagar pelo serviço
+     */
     public void setPaymentType(String paymentType) {
         if (paymentType.equals("transferencia") ||
             paymentType.equals("cartao") ||
@@ -221,14 +343,26 @@ public class OrdemServico {
         }
     }
 
+    /**
+     * Método para obter a nota que o cliente deu para o serviço realizado
+     * @return <i>Int</i> contendo a nota dada pelo cliente
+     */
     public int getClientSatisfaction() {
         return clientSatisfaction;
     }
 
+    /**
+     * Serve para preencher o atributo,<b>clientSatisfaction</b>, que guarda a nota dada pelo cliente pelo serviço prestado
+     * @param clientSatisfaction <i>Int</i> contendo a nota do cliente
+     */
     public void setClientSatisfaction(int clientSatisfaction) {
         this.clientSatisfaction = clientSatisfaction;
     }
 
+    /**
+     * Método para obter o Status em que se encontra a ordem de serviço, seja ela "em espera", "em andamento", "cancelada", "finalizada"
+     * @return <i>String</i> contendo a informação sobre o status da ordem
+     */
     public String getStatus() {
         return status;
     }
