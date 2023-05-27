d8977d628d9b4b8be81f70743c646fbc9a0e399b
Naila <nailasuely@users.noreply.github.com>
2023-05-27 04:30:53 -0300

    💫 feat: Adição de tela de registro.

--- src/main/java/com/example/sistema_gerenciamentofx/Main.java
+++ src/main/java/com/example/sistema_gerenciamentofx/Main.java
@@ -3,17 +3,23 @@ package com.example.sistema_gerenciamentofx;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class Main extends Application{
+
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("login-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Hello!");
         stage.setScene(scene);
+        stage.setResizable(false);
         stage.show();
+        stage.getIcons().add(new Image(getClass().getResourceAsStream("/com/example/sistema_gerenciamentofx/images/1.png")));
+
     }
 
     public static void main(String[] args) {
--- src/main/java/com/example/sistema_gerenciamentofx/controller/LoginController.java
+++ src/main/java/com/example/sistema_gerenciamentofx/controller/LoginController.java
@@ -1,13 +1,23 @@
 package com.example.sistema_gerenciamentofx.controller;
 
+import com.example.sistema_gerenciamentofx.HelloApplication;
 import com.example.sistema_gerenciamentofx.dao.DAO;
 import com.example.sistema_gerenciamentofx.model.Tecnico;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.Stage;
+import com.example.sistema_gerenciamentofx.controller.RegisterController;
+
+import java.io.IOException;
 
 public class LoginController {
 
@@ -31,24 +41,30 @@ public class LoginController {
     //passaword eh o cpf
     @FXML
     void login(ActionEvent event) throws Exception {
-        String passawordText = password1.getText();
+        String passwordText = password1.getText();
         Tecnico tecnico1 = new Tecnico("Italo Sobrenome", "Coité, Bahia",
                 "196.814.670-94", 75);
         DAO.getTecnicoDAO().create(tecnico1);
-        if(!passawordText.isEmpty() && DAO.getTecnicoDAO().findByCPFIsTrue(password1.getText())){
-            System.out.println("pode passar para prox tela");
+        if (!passwordText.isEmpty() && DAO.getTecnicoDAO().findByCPFIsTrue(password1.getText())) {
+            System.out.println("Pode passar para a próxima tela");
+        } else {
+            System.out.println("CPF não existe no sistema ou CPF vazio");
         }
-        else{
-        System.out.println("cpf n existe no sistema ou cpf vazio");}
-
-
-
-
     }
 
     @FXML
-    void showRegisterStage(MouseEvent event) {
+    void showRegisterStage(MouseEvent event) throws IOException {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("register-view.fxml"));
+            Parent root = loader.load();
 
-    }
+            RegisterController registerController = loader.getController();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root));
+            stage.show();
+
+            // Fecha a cena anterior
+            ((Node) event.getSource()).getScene().getWindow().hide();
+        }
 
 }
+
