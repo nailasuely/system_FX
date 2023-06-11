7f9f7286f08747218559dc2e1d72f6f190b9218c
Naila <nailasuely@users.noreply.github.com>
2023-06-11 17:02:26 -0300

    💫 feat: Adição do controller para reports.

--- src/main/java/com/example/sistema_gerenciamentofx/controller/GenerateReportsController.java
+++ src/main/java/com/example/sistema_gerenciamentofx/controller/GenerateReportsController.java
@@ -1,10 +1,18 @@
 package com.example.sistema_gerenciamentofx.controller;
 
+import com.example.sistema_gerenciamentofx.dao.DAO;
+import com.example.sistema_gerenciamentofx.model.Tecnico;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.layout.Pane;
 
-public class GenerateReportsController {
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ResourceBundle;
+
+public class GenerateReportsController implements Initializable {
 
     @FXML
     private Label averageDays;
@@ -27,4 +35,43 @@ public class GenerateReportsController {
     @FXML
     private Label waitingOrders;
 
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        String cpfTecnico = HomeController.getCpfTecnico();
+        String relatorio;
+        try {
+            relatorio = DAO.getTecnicoDAO().findByCPF(cpfTecnico).gerarRelatorioFinal();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        List<String> valoresRelatorio = ReceberValoresRelatorio(relatorio);
+
+        completedServices.setText(valoresRelatorio.get(0));
+        ordersInProgress.setText(valoresRelatorio.get(1));
+        waitingOrders.setText(valoresRelatorio.get(2));
+        invoicing.setText(valoresRelatorio.get(3));
+        averageDays.setText(valoresRelatorio.get(4));
+        customerSatisfaction.setText(valoresRelatorio.get(5));
+
+
+
+    }
+
+    public List<String> ReceberValoresRelatorio(String relatorioString) {
+        List<String> valoresRelatorio = new ArrayList<>();
+
+        String[] linhas = relatorioString.split("\n");
+        for (String linha : linhas) {
+            String[] partes = linha.split(":");
+            if (partes.length == 2) {
+                String valor = partes[1].trim();
+                valoresRelatorio.add(valor);
+            }
+        }
+        for (String valor : valoresRelatorio) {
+            System.out.println(valor);
+        }
+        return valoresRelatorio;
+    }
 }
